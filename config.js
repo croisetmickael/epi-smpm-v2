1cc1ccdf1857f1cfeae5d58757d89143383e5811
croisetmickael <croiset.mickael@gmail.com>
2026-07-14 10:27:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
-/* ============================================================
-   CONFIGURATION — URL du backend Apps Script (déjà renseignée)
-   Les utilisateurs n'ont RIEN à configurer.
-============================================================ */
 window.SMPM_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwbp9a6MHeqxov1xNiMhIxW4z3FzAjdHJZGpbxlyxz8JFjqQDHcbFDV_oX8LE21M2FFTQ/exec"
+  API_URL: "/api/proxy"
 };
